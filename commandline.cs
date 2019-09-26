cf55af0442cf482fe0d787fa539b754b45ff4e7d
Magneticdud <Magneticdud@users.noreply.github.com>
2019-09-26 17:51:37 +0200

    per rextexter
    Così lo uso online

---
+++ commandline.cs
@@ -0,0 +1,29 @@
+//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        int maxvalue = 9;
+        int partizioni = 100;
+        string valorefinale = "";
+        for (int i = 1; i <= maxvalue; i++)
+          {
+            valorefinale += i.ToString() + ",";
+                        for (int j = 1; j < partizioni; j++)
+                        {
+                            valorefinale += (i + maxvalue * j).ToString() + ",";
+                        }
+          } 
+        Console.WriteLine(valorefinale);
+        }
+    }
+}
